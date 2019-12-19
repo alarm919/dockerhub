a6ecc4841ca60145378d77198aa0f7ac01c89251
alarm919 <58713035+alarm919@users.noreply.github.com>
2019-12-19 11:52:48 +0800

    tag GP
    This is a test for dockertest

--- GoodLuck.java
+++ GoodLuck.java
@@ -4,6 +4,9 @@ public class GoodLuck {
 	public static void main(String[] args) {
 		System.out.println("Gook Luck");
 		System.out.println("Gook Luck");
+		System.out.println("Gook Luck");
+		System.out.println("Gook Luck");
+		System.out.println("From GP in 19 Dec,2019");
 	}
 	
 }
